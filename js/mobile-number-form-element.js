950b56b4ef13fc7a6d3d572e70c75c4974f2f536
Nir Yariv <nir@dofinity.com>
2017-05-04 18:53:41 +0000

    Drupal coder fixes

--- js/mobile-number-form-element.js
+++ js/mobile-number-form-element.js
@@ -3,9 +3,7 @@
  */
 
 (function ($) {
-  
   'use strict';
-  
   Drupal.behaviors.mobileNumberFormElement = {
     attach: function (context, settings) {
       $('.mobile-number-field .local-number', context).once('field-setup').each(function () {
@@ -26,7 +24,7 @@
         $input.data('value', val);
         $input.wrap('<div class="country-select"></div>').before('<div class="mobile-number-flag"></div><span class="arrow"></span><div class="prefix"></div>');
         setCountry(val);
-        $input.change(function(e) {
+        $input.change(function (e) {
           if (val !== $(this).val()) {
             val = $(this).val();
             $input.parents('.mobile-number-field').find('.send-button').addClass('show');
@@ -36,7 +34,7 @@
           setCountry(val);
         });
 
-        function setCountry (country) {
+        function setCountry(country) {
           $input.parents('.country-select').find('.mobile-number-flag').removeClass($input.data('value'));
           $input.parents('.country-select').find('.mobile-number-flag').addClass(country.toLowerCase());
           $input.data('value', country.toLowerCase());
